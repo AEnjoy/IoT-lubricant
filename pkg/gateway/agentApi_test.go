4c8082f367de6e125dcdacccc052a464fee665ce
AEnjoy <aenjoyable@163.com>
2024-10-12 15:44:01 +0800

    test(gateway): add test case for app.joinAgent method
    - Implement a new test case for the app.joinAgent method in the gateway package
    - Use mock objects and sync.Map to simulate the application environment
    - Test the entire flow of agent registration and communication with the message queue
    - Verify the correct handling of agent registration, data push, and message push topics
    - Ensure the test completes within a specified timeout period

---
+++ pkg/gateway/agentApi_test.go
@@ -0,0 +1,79 @@
+package gateway
+
+import (
+	"context"
+	"encoding/json"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/AEnjoy/IoT-lubricant/pkg/model"
+	"github.com/AEnjoy/IoT-lubricant/pkg/utils/mq"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPP_JoinAgent(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	mockMqClient := mq.NewMockMq[[]byte](ctrl)
+	deviceList := &sync.Map{}
+
+	ctx, cf := context.WithDeadline(context.Background(), time.Now().Add(8*time.Second))
+	defer cf()
+
+	var success bool
+
+	app := &app{
+		mq:         mockMqClient,
+		ctrl:       ctx,
+		deviceList: deviceList,
+		clientMq: &clientMq{
+			ctrl:       ctx,
+			cancel:     cf,
+			deviceList: deviceList,
+		},
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			assert.True(success)
+			return
+		case <-time.Tick(time.Second):
+			id := uuid.NewString()
+			regCh := make(chan []byte)
+			nilCh := make(chan []byte) // agentDevice dataPush messagePush
+
+			ping, err := json.Marshal(model.Ping{Status: 1})
+			assert.NoError(err)
+
+			// Subscribe Topic_AgentRegister
+			mockMqClient.EXPECT().Subscribe(model.Topic_AgentRegister+id).Return(regCh, nil)
+
+			// handelAgentRegister
+			// send register success to channel
+			go func() {
+				t.Logf("Test: Join %s to Gateway", id)
+				data, err := json.Marshal(model.Register{ID: id})
+				assert.NoError(err)
+				regCh <- data
+			}()
+			mockMqClient.EXPECT().Publish(model.Topic_AgentRegisterAck+id, ping).Return(nil)
+
+			// subscribeDeviceMQ
+			mockMqClient.EXPECT().Subscribe(model.Topic_AgentDevice+id).Return(nilCh, nil)
+
+			// handelAgentDataPush
+			// Subscribe Topic_AgentDataPush
+			mockMqClient.EXPECT().Subscribe(model.Topic_AgentDataPush+id).Return(nilCh, nil)
+
+			// Subscribe Topic_MessagePush
+			mockMqClient.EXPECT().Subscribe(model.Topic_MessagePush+id).Return(nilCh, nil)
+
+			assert.NoError(app.joinAgent(id))
+			success = true
+		}
+	}
+}
