e18269d0220f2962b905c4682ffd7f003c55cf8b
AEnjoy <aenjoyable@163.com>
2024-10-21 01:12:10 +0800

    feat(task): add gateway management request structures
    - Introduce new package `task` to handle gateway-related requests
    - Define `AddGatewayRequest` and `RemoveGatewayRequest` structs
    - Implement `HostInfo` and `PreCoreInfo` structs for different connection ways- Include `Way` type and related constants to specify connection methods
    - Add `crypto.Tls` import for TLS configuration in `PreCoreInfo`

---
+++ pkg/types/task/gateway.go
@@ -0,0 +1,39 @@
+package task
+
+import (
+	"github.com/AEnjoy/IoT-lubricant/pkg/types/crypto"
+)
+
+type Way uint8
+
+const (
+	WayGatewayHostInfo Way = iota
+	WayPreCoreInfo
+)
+
+type AddGatewayRequest struct {
+	Way    Way `json:"way"`
+	Config any `json:"config"` // HostInfo or PreCoreInfo
+}
+
+// 网关设备连接信息
+type HostInfo struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"` //todo: 密码加密
+}
+
+// 预置Core连接信息
+type PreCoreInfo struct {
+	GatewayID string     `json:"gateway_id"`
+	Host      string     `json:"host"`
+	Port      int        `json:"port"`
+	Tls       bool       `json:"tls"`
+	TlsConfig crypto.Tls `json:"tls_config"`
+}
+
+type RemoveGatewayRequest struct {
+	ID          string `json:"id"`
+	RemoveAgent bool   `json:"remove_agent"`
+}
