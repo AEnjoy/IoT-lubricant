d4563263ba399335179858298b673397d7f1a4b5
AEnjoy <aenjoyable@163.com>
2024-11-27 22:51:26 +0800

    fix(edge): update gRPC client to use insecure credentials

--- pkg/edge/grpc.go
+++ pkg/edge/grpc.go
@@ -3,10 +3,11 @@ package edge
 import (
 	"github.com/AEnjoy/IoT-lubricant/protobuf/agent"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func NewAgentCli(address string) (agent.EdgeServiceClient, error) {
-	conn, err := grpc.NewClient(address)
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
