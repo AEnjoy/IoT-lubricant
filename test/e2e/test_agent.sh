9b8ebc4315dbdd9a6c9ca0499ddf3f4dfe074b5c
AEnjoy <aenjoyable@163.com>
2024-12-14 16:37:57 +0800

    ci(e2e): add end-to-end test for agent
    - Add E2E test step in GitHub Actions workflow
    - Create test_agent.sh script for running end-to-end tests
    - Include setup of Python dependencies and mock server
    - Build and run the agent for testing

---
+++ test/e2e/test_agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+echo "Test Agent..."
+
+echo "Preparing python dependency"
+pip3 install -i test/mock_driver/clock/requirements.txt
+
+echo "Running Mock E2E test server for openapi"
+python3 test/mock_driver/clock/clock.py &
+
+echo "Start Agent:"
+cd cmd/agent
+go build . -o agent
+./agent --env=core.env &
+
+echo "Start Test Client:"
+cd ../test/agent
+go build . -o agent_test
+cd ../../test
+../cmd/test/agent/agent_test mini --agent-id clock-agent
+
