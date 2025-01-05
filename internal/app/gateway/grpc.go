b994acbd55cf885b571141a786ca1f948a5ab3e8
AEnjoy <aenjoyable@163.com>
2025-01-06 00:44:21 +0800

    feat(gateway): implement task handling and streaming functionality
    - Add task sending and receiving logic in separate goroutines
    - Implement error handling for gRPC stream errors
    - Add support for multiple task types, including CorePushTask and CoreQueryTaskResult
    - Refactor task handling logic for better modularity and readability- Improve logging for task-related events and errors

--- internal/app/gateway/grpc.go
+++ internal/app/gateway/grpc.go
@@ -1,17 +1,15 @@
 package gateway
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"net"
 	"time"
 
-	"github.com/AEnjoy/IoT-lubricant/internal/model"
 	"github.com/AEnjoy/IoT-lubricant/pkg/logger"
-	"github.com/AEnjoy/IoT-lubricant/pkg/types"
-	taskTypes "github.com/AEnjoy/IoT-lubricant/pkg/types/task"
 	"github.com/AEnjoy/IoT-lubricant/protobuf/core"
 	"github.com/AEnjoy/IoT-lubricant/protobuf/meta"
-	json "github.com/bytedance/sonic"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -25,6 +23,7 @@ func (a *app) grpcApp() error {
 	retryAttempts := 3        // 最大重试次数
 	retryDelay := time.Second // 初始重试延迟
 
+	// task
 	for i := 0; i < retryAttempts; i++ {
 		task, err := a.grpcClient.GetTask(a.ctrl)
 		if err != nil {
@@ -33,13 +32,53 @@ func (a *app) grpcApp() error {
 				retryDelay *= 2 // 指数退避
 				continue        // 重试
 			}
-			return fmt.Errorf("GetTask failed after %d attempts: %w", retryAttempts, err)
+			return fmt.Errorf("GetTask failed after %d attempts: %w\n", retryAttempts, err)
 		}
+
+		// send - async task result
+		go func() {
+			for taskId := range a.task.GetNotifyCh() {
+				err := task.Send(&core.Task{
+					ID: taskId,
+					Task: &core.Task_CoreQueryTaskResultResponse{
+						CoreQueryTaskResultResponse: &core.QueryTaskResultResponse{
+							TaskId: taskId,
+							Result: a.task.Query(taskId).GetResult(),
+						},
+					},
+				})
+				if err != nil {
+					st, ok := status.FromError(err)
+					if ok {
+						if st.Code() == codes.Unavailable || st.Code() == codes.Canceled || st.Code() == codes.DeadlineExceeded || err == io.EOF {
+							logger.Errorf("Send goroutine exiting due to error: %v", err)
+							return
+						} else {
+							logger.Errorf("Unrecoverable gRPC Send error: %v", err)
+						}
+					} else {
+						logger.Errorf("Send error: %v", err)
+						if errors.Is(err, io.EOF) {
+							logger.Errorln("grpc stream closed")
+							return
+						}
+						if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+							logger.Errorln("net timeout")
+						}
+						return
+					}
+					return
+				}
+				a.task.RemoveResult(taskId)
+			}
+		}()
+
+		// recv{
 
 		for {
 			resp, err := task.Recv()
 			if err == io.EOF {
-				logger.Info("grpc stream closed")
+				logger.Errorln("grpc stream closed")
 				return nil
 			}
 
@@ -62,27 +101,79 @@ func (a *app) grpcApp() error {
 					logger.Errorf("Receive error: %v", err)
 					return fmt.Errorf("recv failed: %w", err)
 				}
-				return err
 			}
-
-			var c types.TaskCommand
-			//var taskId string
 
 			switch t := resp.GetTask().(type) {
 			case *core.Task_GatewayGetTaskResponse:
-				//content := task.GatewayGetTaskResponse.GetMessage().GetContent()
-				//taskId = task.GatewayGetTaskResponse.GetMessage().GetTaskId()
-				//err = json.Unmarshal(content, &c)
-				//if err != nil {
-				//	return err
-				//}
+				a.handelGatewayGetTaskResponse(t)
 			case *core.Task_CorePushTaskRequest:
 				resp := a.handelCorePushTaskAsync(t)
-				task.Send(&core.Task{ID: resp.TaskId,
+				err := task.Send(&core.Task{ID: resp.TaskId,
 					Task: &core.Task_CorePushTaskResponse{
 						CorePushTaskResponse: resp,
 					},
 				})
+				if err != nil {
+					st, ok := status.FromError(err)
+					if ok {
+						if st.Code() == codes.Unavailable || st.Code() == codes.Canceled || st.Code() == codes.DeadlineExceeded || err == io.EOF {
+							logger.Errorf("Send goroutine exiting due to error: %v", err)
+							continue
+						} else {
+							logger.Errorf("Unrecoverable gRPC Send error: %v", err)
+						}
+					} else {
+						logger.Errorf("Send error: %v", err)
+						if errors.Is(err, io.EOF) {
+							logger.Errorln("grpc stream closed")
+							continue
+						}
+						if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+							logger.Errorln("net timeout")
+						}
+						continue
+					}
+					continue
+				}
+				a.task.RemoveResult(resp.GetTaskId())
+			case *core.Task_CoreQueryTaskResultRequest:
+				resp := a.task.Query(t.CoreQueryTaskResultRequest.GetTaskId())
+				err := task.Send(&core.Task{ID: resp.TaskId,
+					Task: &core.Task_CoreQueryTaskResultResponse{
+						CoreQueryTaskResultResponse: resp,
+					},
+				})
+				if err != nil {
+					st, ok := status.FromError(err)
+					if ok {
+						if st.Code() == codes.Unavailable || st.Code() == codes.Canceled || st.Code() == codes.DeadlineExceeded || err == io.EOF {
+							logger.Errorf("Send goroutine exiting due to error: %v", err)
+							continue
+						} else {
+							logger.Errorf("Unrecoverable gRPC Send error: %v", err)
+						}
+					} else {
+						logger.Errorf("Send error: %v", err)
+						if errors.Is(err, io.EOF) {
+							logger.Errorln("grpc stream closed")
+							continue
+						}
+						if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+							logger.Errorln("net timeout")
+						}
+						continue
+					}
+					continue
+				}
+				if resp.GetFinish() != nil {
+					a.task.RemoveResult(resp.GetTaskId())
+				}
+			case *core.Task_GatewayQueryTaskResultResponse:
+			// todo:
+			case *core.Task_NoTaskResponse:
+				logger.Infoln("gateway get task request success, and no task need to execute")
+			case *core.Task_ErrorMessage:
+				logger.Errorf("gateway send request to core success,but get the error: %s", t.ErrorMessage.String())
 			}
 
 			//switch c.ID {
---
+++ internal/app/gateway/task_handel.go
@@ -0,0 +1,10 @@
+package gateway
+
+import (
+	"github.com/AEnjoy/IoT-lubricant/internal/cache"
+	"github.com/AEnjoy/IoT-lubricant/protobuf/core"
+)
+
+func (a *app) handelTask(task *core.TaskDetail, c *cache.MemoryCache[*core.QueryTaskResultResponse]) {
+	// todo:impl me
+}
