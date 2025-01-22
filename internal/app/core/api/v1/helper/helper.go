567ed2d4a9d45cb4a16956635f5181a2487bd1d7
AEnjoy <aenjoyable@163.com>
2025-01-22 19:36:32 +0800

    refactor(api): add response helper functions for consistent error handling
    - Introduce helper package with functions for successful and failed responses
    - Implement Success, FailedByServer, and FailedByClient functions
    - Use Exception type from exception package for error handling
    - Ensure consistent JSON response format for errors

---
+++ internal/app/core/api/v1/helper/helper.go
@@ -0,0 +1,35 @@
+package helper
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/AEnjoy/IoT-lubricant/pkg/types/exception"
+	"github.com/gin-gonic/gin"
+)
+
+func Success(data any, c *gin.Context) {
+	c.JSON(http.StatusOK, data)
+}
+func FailedByServer(err error, c *gin.Context) {
+	// 500 状态, 接口报错, 返回内容: Exception对象
+	httpCode := http.StatusInternalServerError
+	_failed(httpCode, err, c)
+}
+func FailedByClient(err error, c *gin.Context) {
+	// 400 状态, 接口报错, 返回内容: ApiException对象
+	httpCode := http.StatusBadRequest
+	_failed(httpCode, err, c)
+}
+func _failed(httpCode int, err error, c *gin.Context) {
+	var v *exception.Exception
+	if errors.As(err, &v) {
+		if v.Code != 0 {
+			httpCode = int(v.Code)
+		}
+		c.JSON(httpCode, v)
+	} else {
+		c.JSON(httpCode, err)
+	}
+	c.Abort()
+}
