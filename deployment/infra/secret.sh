35efb7d6280dcf71c62bd588e2cca7532910d52a
AEnjoy <aenjoyable@163.com>
2025-01-30 17:38:49 +0800

    ci(k8s): update auth callback URL for lubricant-core service
    - Replace localhost callback URL with Kubernetes service URL
    - Update redirectUri to http://lubricant-core.lubricant.svc.cluster.local:8080/api/v1/signin

--- deployment/infra/secret.sh
+++ deployment/infra/secret.sh
@@ -3,4 +3,4 @@ set -e
 kubectl create ns lubricant
 kubectl create secret generic lubricant-secrets -n lubricant\
   --from-literal=DB_PASSWORD='123456' \
-  --from-literal=AUTH_CLIENT_SECRET='<your-client-secret>'
+  --from-literal=AUTH_CLIENT_SECRET='dd9657c7b8cc10a72f77b283253b3a0a31b91175'
