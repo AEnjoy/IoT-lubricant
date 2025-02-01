012813ddf9c31c8096d726c6c2f02718518d79f9
AEnjoy <aenjoyable@163.com>
2025-02-02 00:48:05 +0800

    ci(deployment): reorganize CI workflow and add NATS deployment
    - Refactor CI workflow to improve efficiency and clarity
    - Add NATS deployment to the database namespace
    - Remove redundant namespace creation commands
    - Update secret creation to avoid unnecessary namespace creation
    - Reorder workflow steps for better logical flow

--- deployment/infra/secret.sh
+++ deployment/infra/secret.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-kubectl create ns lubricant
 kubectl create secret generic lubricant-secrets -n lubricant\
   --from-literal=DB_PASSWORD='123456' \
   --from-literal=AUTH_CLIENT_SECRET='dd9657c7b8cc10a72f77b283253b3a0a31b91175'
