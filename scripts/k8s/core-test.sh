eaaf71d8506bb529e59b8feeb822b49b96d3ce00
luyicheng <aenjoyable@163.com>
2025-02-25 18:29:11 +0800

    test(core): 优化 Kubernetes端到端测试脚本
    - 在 core-test.sh 中添加 pod_name 参数，用于获取指定 pod 日志

--- scripts/k8s/core-test.sh
+++ scripts/k8s/core-test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+pod_name=$1
+
 LOGIN_URL="http://casdoor-service.auth-core.svc.cluster.local:8000/api/login?clientId=6551a3584403d5264584&responseType=code&redirectUri=http%3A%2F%2Flubricant-core.lubricant.svc.cluster.local%3A8080%2Fapi%2Fv1%2Fsignin&type=code&scope=read&state=casdoor&nonce=&code_challenge_method=&code_challenge="
 CALLBACK_URL="http://lubricant-core.lubricant.svc.cluster.local:8080/api/v1/signin"
 USER_INFO_URL="http://lubricant-core.lubricant.svc.cluster.local:8080/api/v1/user/info"
@@ -32,6 +35,7 @@ response=$(curl -s -X GET -c "$COOKIE_FILE" \
 if [ $? -ne 0 ]; then
   echo "Error: Failed to get cookie curl request failed"
   echo "Response: $response"
+  kubectl logs "$pod_name" -n lubricant
   exit 1
 fi
 
@@ -40,12 +44,14 @@ msg=$(echo "$response" | jq -r '.msg')
 if [ "$msg" != "success" ]; then
   echo "Error: Login failed, msg=$msg"
   echo "Response: $response"
+  kubectl logs "$pod_name" -n lubricant
   exit 1
 fi
 
 if [ ! -f "$COOKIE_FILE" ]; then
   echo "Error: Failed to get cookie"
   echo "File $COOKIE_FILE does not exist"
+  kubectl logs "$pod_name" -n lubricant
   exit 1
 fi
 echo "Cookie saved to $COOKIE_FILE"
@@ -56,5 +62,6 @@ echo "Getting user info..."
 curl -s -X GET -b "$COOKIE_FILE" "$USER_INFO_URL"
 if [ $? -ne 0 ]; then
   echo "Error: Failed to get user info"
+  kubectl logs "$pod_name" -n lubricant
   exit 1
 fi
