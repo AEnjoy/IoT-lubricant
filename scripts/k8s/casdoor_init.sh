5d8a128b63ba644d8c70731c4a80af7d03d88a04
luyicheng <aenjoyable@163.com>
2025-02-25 16:26:20 +0800

    ci: 更新 Casdoor 初始化脚本和错误检查功能

--- scripts/k8s/casdoor_init.sh
+++ scripts/k8s/casdoor_init.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# there is not jq command at nginx pod
 apt-get update && apt-get install -y jq
 
 CASDOOR_URL='http://casdoor-service.auth-core.svc.cluster.local:8000'
 
-# 创建应用
+# Create App
 curl -X POST "$CASDOOR_URL/api/add-application?username=built-in/admin&password=123" \
  -H "Content-Type: application/json" -d '@create_app.json'
 
-# 查询应用详情并提取证书
+# Get Cert
 curl -s "$CASDOOR_URL/api/get-cert?id=admin/cert-built-in&username=built-in/admin&password=123" | jq -r '.data.certificate' > ./crt.pem
-
-# cp ./crt.pem /etc/casdoor/public.pem
