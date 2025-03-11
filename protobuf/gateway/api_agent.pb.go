e1dc15eee261e82a39b14ab7d5ea9f81382fdb2b
luyicheng <aenjoyable@163.com>
2025-03-11 11:29:52 +0800

    feat(coreService): 新增任务结果响应类型
    - 在 coreService_gateway.proto 文件中添加了 TaskResultResponse 消息类型
    - 在 QueryResponse 消息的 oneof resp 中添加了 taskResult 字段
    - 这些变更将用于处理任务结果查询相关的响应

--- protobuf/gateway/api_agent.pb.go
+++ protobuf/gateway/api_agent.pb.go
@@ -450,6 +450,42 @@ func (x *StopGatherRequest) GetAgentId() string {
 	return ""
 }
 
+type ReloadAgentControlRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *ReloadAgentControlRequest) Reset() {
+	*x = ReloadAgentControlRequest{}
+	mi := &file_protobuf_gateway_api_agent_proto_msgTypes[9]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ReloadAgentControlRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ReloadAgentControlRequest) ProtoMessage() {}
+
+func (x *ReloadAgentControlRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_protobuf_gateway_api_agent_proto_msgTypes[9]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ReloadAgentControlRequest.ProtoReflect.Descriptor instead.
+func (*ReloadAgentControlRequest) Descriptor() ([]byte, []int) {
+	return file_protobuf_gateway_api_agent_proto_rawDescGZIP(), []int{9}
+}
+
 var File_protobuf_gateway_api_agent_proto protoreflect.FileDescriptor
 
 var file_protobuf_gateway_api_agent_proto_rawDesc = string([]byte{
@@ -494,9 +530,11 @@ var file_protobuf_gateway_api_agent_proto_rawDesc = string([]byte{
 	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x67, 0x65, 0x6e, 0x74, 0x49, 0x64, 0x22, 0x2d, 0x0a,
 	0x11, 0x53, 0x74, 0x6f, 0x70, 0x47, 0x61, 0x74, 0x68, 0x65, 0x72, 0x52, 0x65, 0x71, 0x75, 0x65,
 	0x73, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x67, 0x65, 0x6e, 0x74, 0x49, 0x64, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x67, 0x65, 0x6e, 0x74, 0x49, 0x64, 0x42, 0x12, 0x5a, 0x10,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x67, 0x65, 0x6e, 0x74, 0x49, 0x64, 0x22, 0x1b, 0x0a, 0x19,
+	0x52, 0x65, 0x6c, 0x6f, 0x61, 0x64, 0x41, 0x67, 0x65, 0x6e, 0x74, 0x43, 0x6f, 0x6e, 0x74, 0x72,
+	0x6f, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x42, 0x12, 0x5a, 0x10, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 })
 
 var (
@@ -511,27 +549,28 @@ func file_protobuf_gateway_api_agent_proto_rawDescGZIP() []byte {
 	return file_protobuf_gateway_api_agent_proto_rawDescData
 }
 
-var file_protobuf_gateway_api_agent_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
+var file_protobuf_gateway_api_agent_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
 var file_protobuf_gateway_api_agent_proto_goTypes = []any{
-	(*StartAgentRequest)(nil),     // 0: lubricant.gateway.StartAgentRequest
-	(*StopAgentRequest)(nil),      // 1: lubricant.gateway.StopAgentRequest
-	(*RemoveAgentRequest)(nil),    // 2: lubricant.gateway.RemoveAgentRequest
-	(*UpdateAgentRequest)(nil),    // 3: lubricant.gateway.UpdateAgentRequest
-	(*EditAgentRequest)(nil),      // 4: lubricant.gateway.EditAgentRequest
-	(*CreateAgentRequest)(nil),    // 5: lubricant.gateway.CreateAgentRequest
-	(*GetAgentStatusRequest)(nil), // 6: lubricant.gateway.GetAgentStatusRequest
-	(*StartGatherRequest)(nil),    // 7: lubricant.gateway.StartGatherRequest
-	(*StopGatherRequest)(nil),     // 8: lubricant.gateway.StopGatherRequest
-	(*agent.AgentInfo)(nil),       // 9: lubricant.agent.AgentInfo
+	(*StartAgentRequest)(nil),         // 0: lubricant.gateway.StartAgentRequest
+	(*StopAgentRequest)(nil),          // 1: lubricant.gateway.StopAgentRequest
+	(*RemoveAgentRequest)(nil),        // 2: lubricant.gateway.RemoveAgentRequest
+	(*UpdateAgentRequest)(nil),        // 3: lubricant.gateway.UpdateAgentRequest
+	(*EditAgentRequest)(nil),          // 4: lubricant.gateway.EditAgentRequest
+	(*CreateAgentRequest)(nil),        // 5: lubricant.gateway.CreateAgentRequest
+	(*GetAgentStatusRequest)(nil),     // 6: lubricant.gateway.GetAgentStatusRequest
+	(*StartGatherRequest)(nil),        // 7: lubricant.gateway.StartGatherRequest
+	(*StopGatherRequest)(nil),         // 8: lubricant.gateway.StopGatherRequest
+	(*ReloadAgentControlRequest)(nil), // 9: lubricant.gateway.ReloadAgentControlRequest
+	(*agent.AgentInfo)(nil),           // 10: lubricant.agent.AgentInfo
 }
 var file_protobuf_gateway_api_agent_proto_depIdxs = []int32{
-	9, // 0: lubricant.gateway.EditAgentRequest.info:type_name -> lubricant.agent.AgentInfo
-	9, // 1: lubricant.gateway.CreateAgentRequest.info:type_name -> lubricant.agent.AgentInfo
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	10, // 0: lubricant.gateway.EditAgentRequest.info:type_name -> lubricant.agent.AgentInfo
+	10, // 1: lubricant.gateway.CreateAgentRequest.info:type_name -> lubricant.agent.AgentInfo
+	2,  // [2:2] is the sub-list for method output_type
+	2,  // [2:2] is the sub-list for method input_type
+	2,  // [2:2] is the sub-list for extension type_name
+	2,  // [2:2] is the sub-list for extension extendee
+	0,  // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_protobuf_gateway_api_agent_proto_init() }
@@ -547,7 +586,7 @@ func file_protobuf_gateway_api_agent_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_protobuf_gateway_api_agent_proto_rawDesc), len(file_protobuf_gateway_api_agent_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   9,
+			NumMessages:   10,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
