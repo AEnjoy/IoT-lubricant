d74a2540027f631e07fc5b2ece974afa0317070c
AEnjoy <aenjoyable@163.com>
2025-03-26 00:11:46 +0800

    refactor(version): implement version information management in a separate package
    - Create a new `version` package to centralize version information
    - Move version-related variables and functions to the `version` package
    - Update `main` packages in `agent`, `gateway`, and `lubricant` to use the new `version` package
    - Remove redundant `start.go` files that contained version information
    - Update `Makefile` to reflect changes in build targets

---
+++ pkg/version/version.go
@@ -0,0 +1,48 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+)
+
+var (
+	ServiceName       string
+	Version           string
+	BuildTime         string
+	GoVersion         string
+	GitCommit         string
+	Features          string
+	BuildHostPlatform string
+	PlatformVersion   string
+)
+
+func PrintVersionInfo() {
+	fmt.Printf("%s-Version: %s\n", ServiceName, Version)
+	fmt.Printf("Build-Time: %s\n", BuildTime)
+	fmt.Printf("Go-Version: %s\n", GoVersion)
+	fmt.Printf("Git-Commit: %s\n", GitCommit)
+	fmt.Printf("Features: %s\n", Features)
+	fmt.Printf("BuildHostPlatform: %s\n", BuildHostPlatform)
+	fmt.Printf("BuildPlatformVersion: %s\n", PlatformVersion)
+	fmt.Printf("Runing Platform Info: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+}
+func MarshallJsonString() string {
+	return fmt.Sprintf(`{"serviceName":"%s","version":"%s","buildTime":"%s","goVersion":"%s","gitCommit":"%s","features":"%s","buildHostPlatform":"%s","buildPlatformVersion":"%s","runingPlatformInfo":"%s/%s"}`,
+		ServiceName,
+		Version,
+		BuildTime,
+		GoVersion,
+		GitCommit,
+		Features,
+		BuildHostPlatform,
+		PlatformVersion,
+		runtime.GOOS,
+		runtime.GOARCH)
+}
+func MarshallJson() []byte {
+	return StringToBytes(MarshallJsonString())
+}
+func StringToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
+}
