4f2c969f3dbf8ff6572e13851171bd315677217b
AEnjoy <aenjoyable@163.com>
2025-03-25 23:48:52 +0800

    feat(logg): implement log struct and svc
    - Define Log struct with necessary fields for logging
    - Implement basic log processing in svc.go
    - Add imports for exception codes and UUID generation

--- pkg/model/logs.go
+++ pkg/model/logs.go
@@ -1,8 +1,31 @@
 package model
 
+import (
+	"time"
+
+	exceptionCode "github.com/aenjoy/iot-lubricant/pkg/types/exception/code"
+	svcpb "github.com/aenjoy/iot-lubricant/protobuf/svc"
+)
+
 type Log struct {
 	ID         int    `json:"id" gorm:"column:id;primary_key;autoIncrement"`
 	LogID      string `json:"logId" gorm:"column:log_id"`
 	OperatorID string `json:"operatorId" gorm:"column:operator_id"` // UserID / DevicesID
-	// todo
+
+	ServiceName      string                `json:"serviceName" gorm:"column:service_name"`
+	Level            svcpb.Level           `json:"level" gorm:"column:level;type:tinyint"`
+	IPAddress        string                `json:"ipAddress" gorm:"column:ip_address"`
+	Protocol         string                `json:"protocol" gorm:"column:protocol"`
+	Action           string                `json:"action" gorm:"column:action"`
+	OperationType    svcpb.Operation       `json:"operationType" gorm:"column:operation_type;type:tinyint"`
+	Cost             int64                 `json:"cost" gorm:"column:cost;type:timestamp"`
+	Message          string                `json:"message" gorm:"column:message"`
+	ServiceErrorCode exceptionCode.ResCode `json:"serviceErrorCode" gorm:"column:service_error_code;type:int"`
+	Metadata         string                `json:"metadata" gorm:"column:metadata;type:json"`
+	ExceptionInfo    string                `json:"exceptionInfo" gorm:"column:exception_info;type:json"`
+	Time             time.Time             `json:"time" gorm:"column:time;type:timestamp"`
+}
+
+func (Log) TableName() string {
+	return "logs"
 }
