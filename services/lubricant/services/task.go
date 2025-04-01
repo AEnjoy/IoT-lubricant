f0b4f483943da69dbcd9c7e35eab384227c9e811
luyicheng <aenjoyable@163.com>
2025-04-01 17:05:25 +0800

    refactor(task): 重构任务处理逻辑
    - 引入 taskArgs 结构体封装任务参数，提高代码可读性和可维护性
    - 优化任务处理流程，移除冗余代码
    -调整任务主题生成逻辑，使其更具可扩展性
    - 添加日志输出，便于调试和追踪任务处理过程

--- services/lubricant/services/task.go
+++ services/lubricant/services/task.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"fmt"
 
-	taskTypes "github.com/aenjoy/iot-lubricant/pkg/types/task"
+	"github.com/aenjoy/iot-lubricant/pkg/types/task"
 	"github.com/aenjoy/iot-lubricant/pkg/types/user"
 	"google.golang.org/protobuf/proto"
 )
 
 func (a *AgentService) PushTaskAgent(ctx context.Context, taskid *string, userID, gatewayID, agentID string, bin []byte) (string, string, error) {
-	return _taskHelper(
-		ctx,
-		a.txnHelper,
-		a.store.Mq,
-		a.db.AddAsyncJob,
-		taskid,
-		user.RoleAgent,
-		gatewayID, //agentID,
-		userID,
-		string(taskTypes.TargetAgent),
-		fmt.Sprintf("/task/%s/%s", userID, taskTypes.TargetGateway), //fmt.Sprintf("/task/%s/%s/%s", taskTypes.TargetGateway, gatewayID, taskTypes.TargetAgent),
-		bin,
-	)
+	return _taskHelper(&taskArgs{
+		ctx:           ctx,
+		txnHelper:     a.txnHelper,
+		storeMq:       a.store.Mq,
+		dbAddAsyncJob: a.db.AddAsyncJob,
+		taskID:        taskid,
+		executorType:  user.RoleAgent,
+		executorID:    gatewayID,
+		userID:        userID,
+		targetName:    task.TargetGateway,
+		topicPrefix:   fmt.Sprintf("/task/%s", userID),
+		bin:           bin,
+	})
 }
 
 // PushTaskAgentPb :
@@ -36,19 +36,19 @@ func (a *AgentService) PushTaskAgentPb(ctx context.Context, taskid *string, user
 }
 
 func (s *GatewayService) PushTask(ctx context.Context, taskid *string, gatewayID, userID string, bin []byte) (string, string, error) {
-	return _taskHelper(
-		ctx,
-		s.txnHelper,
-		s.store.Mq,
-		s.db.AddAsyncJob,
-		taskid,
-		user.RoleGateway,
-		gatewayID,
-		userID,
-		string(taskTypes.TargetGateway),
-		fmt.Sprintf("/task/%s/%s", userID, taskTypes.TargetGateway),
-		bin,
-	)
+	return _taskHelper(&taskArgs{
+		ctx:           ctx,
+		txnHelper:     s.txnHelper,
+		storeMq:       s.store.Mq,
+		dbAddAsyncJob: s.db.AddAsyncJob,
+		taskID:        taskid,
+		executorType:  user.RoleGateway,
+		executorID:    gatewayID,
+		userID:        userID,
+		targetName:    task.TargetGateway,
+		topicPrefix:   fmt.Sprintf("/task/%s", userID),
+		bin:           bin,
+	})
 }
 func (s *GatewayService) PushTaskPb(ctx context.Context, taskid *string, userID, gatewayID string, pb proto.Message) (string, string, error) {
 	bin, err := proto.Marshal(pb)
