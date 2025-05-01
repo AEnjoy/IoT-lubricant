72208d04bec03b60b3c1ce6edcc04e19b58c6ad2
AEnjoy <aenjoyable@163.com>
2025-05-02 00:20:00 +0800

    refactor(mq): update constants and add TODO comment
    - Update constant definitions in mq.go
    - Add TODO comment for potential refactor of DATASTORE_USER constant

--- pkg/constant/mq.go
+++ pkg/constant/mq.go
@@ -3,7 +3,7 @@ package constant
 // logger
 const (
 	MESSAGE_SVC_LOGGER      = "/logger/message"
-	DATASTORE_USER          = "/handler/data/userid"
+	DATASTORE_USER          = "/handler/data/userid" // todo: refactor: 应该使用 projectId 而不是 userId
 	DATASTORE_USER_REG_RESP = DATASTORE_USER + "/%s/reg/response"
 	DATASTORE_USER_CLOSE    = DATASTORE_USER + "/%s/close"
 	DATASTORE_USER_DATA     = DATASTORE_USER + "/%s/data"
