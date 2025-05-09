738bbd5733c8a4c859cf44f6b44c39825840a745
luyicheng <aenjoyable@163.com>
2025-05-09 16:54:14 +0800

    feat(datastore): implement project handling in handel function with subscription and data processing

--- services/datastoreAssistant/handel.go
+++ services/datastoreAssistant/handel.go
@@ -1,5 +1,97 @@
 package datastoreAssistant
 
-func (a *app) handel(userIdStr any) {
+import (
+	"fmt"
 
+	"github.com/aenjoy/iot-lubricant/pkg/constant"
+	"github.com/aenjoy/iot-lubricant/pkg/model"
+	"github.com/aenjoy/iot-lubricant/pkg/utils/compress"
+	"github.com/aenjoy/iot-lubricant/services/datastoreAssistant/driver"
+	logg "github.com/aenjoy/iot-lubricant/services/logg/api"
+
+	"github.com/bytedance/sonic"
+	"github.com/cloudwego/base64x"
+)
+
+func (a *app) handel(projectIdStr any) {
+	topic := fmt.Sprintf(constant.DATASTORE_PROJECT_DATA, projectIdStr)
+	subscribeCh, err := a.DataStore.V2mq.QueueSubscribe(topic)
+	if err != nil {
+		logg.L.Errorf("Failed to subscribe to topic %s: %s", projectIdStr, err)
+		return
+	}
+	defer a.DataStore.V2mq.QueueUnsubscribe(topic)
+
+	project, err := a.DataStore.GetProject(a.Ctx, projectIdStr.(string))
+	if err != nil {
+		logg.L.Errorf("Failed to get project by projectId %s: %s", projectIdStr, err)
+		return
+	}
+
+	txn := a.DataStore.Begin()
+	agents, err := a.DataStore.GetAgentsByProjectID(a.Ctx, txn, projectIdStr.(string))
+	a.DataStore.Commit(txn)
+	if err != nil {
+		logg.L.Errorf("Failed to get agents by projectId %s: %s", projectIdStr, err)
+		return
+	}
+	if len(agents) < 1 {
+		logg.L.Errorf("No agents found for projectId %s", projectIdStr)
+		return
+	}
+	agent := agents[0]
+
+	decompressor, err := compress.NewCompressor(agent.Algorithm)
+	if err != nil {
+		logg.L.Errorf("Failed to create decompressor: %s", err)
+		return
+	}
+
+	engine, err := a.DataStore.GetEngineByProjectID(a.Ctx, projectIdStr.(string))
+	if err != nil {
+		logg.L.Errorf("Failed to get engine by projectId %s: %s", projectIdStr, err)
+		return
+	}
+	var (
+		dri    driver.IDriver
+		closer func() error
+	)
+
+	switch engine.DataBaseType {
+	case "mysql":
+		dsn, _ := base64x.StdEncoding.DecodeString(engine.DSN)
+		dri, closer, err = driver.NewMySQLDriver(string(dsn), engine.Table, project.UserID)
+	case "TDEngine":
+		dsn, _ := base64x.StdEncoding.DecodeString(engine.DSN)
+		var tdmodel model.LinkerInfo
+		_ = sonic.Unmarshal(dsn, &tdmodel)
+		dri, closer, err = driver.NewTDEngineDriver(
+			project.UserID, tdmodel.Host, tdmodel.User,
+			tdmodel.Pass, tdmodel.Db, tdmodel.Port,
+			&engine.Table, tdmodel.Schemaless,
+		)
+	default:
+		logg.L.Errorf("Unsupported database type %s", engine.DataBaseType)
+		return
+	}
+	if err != nil {
+		logg.L.Errorf("Failed to create driver: %s userId:%s projectId:%s", err, project.UserID, projectIdStr)
+		return
+	}
+	defer closer()
+
+	for {
+		select {
+		case <-a.Ctx.Done():
+			logg.L.Infof("Context done, exiting...")
+			return
+		case data := <-subscribeCh:
+			deData, err := decompressor.Decompress(data.([]byte))
+			if err != nil {
+				logg.L.Errorf("Failed to decompress data: %s", err)
+				continue
+			}
+			dri.Write(deData)
+		}
+	}
 }
