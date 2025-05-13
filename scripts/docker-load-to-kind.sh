8018a1704d2ad369e291192ddd17af31808721ea
luyicheng <aenjoyable@163.com>
2025-05-14 00:25:06 +0800

    fix: downgrade tdengine image to 3.3.5.2 for compatibility

--- scripts/docker-load-to-kind.sh
+++ scripts/docker-load-to-kind.sh
@@ -7,6 +7,6 @@ kind load docker-image redis:7.4.2 &
 kind load docker-image bitnami/mysql:8.4.4-debian-12-r10 &
 kind load docker-image casbin/casdoor:v1.854.0 &
 kind load docker-image python:3.9-slim-buster &
-kind load docker-image tdengine/tdengine:3.3.6.3 &
+kind load docker-image tdengine/tdengine:3.3.5.2 &
 kind load docker-image registry.k8s.io/etcd:3.5.21-0 &
 wait
--- scripts/docker-save.sh
+++ scripts/docker-save.sh
@@ -4,11 +4,11 @@
 echo "Cache: Saving Docker Images..."
 mkdir -p /tmp/docker-images
 docker save \
-    nginx:1.27 alpine:3.21.3 kindest/node:v1.32.2 \
+    nginx:1.27 alpine:3.21.3 kindcest/node:v1.32.2 \
     nats:2.10.26 redis:7.4.2 casbin/casdoor:v1.854.0 \
     python:3.9-slim-buster golang:1.24-alpine \
     bitnami/mysql:8.4.4-debian-12-r10 \
-    tdengine/tdengine:3.3.6.3 \
+    tdengine/tdengine:3.3.5.2 \
     registry.k8s.io/etcd:3.5.21-0 \
     aenjoy/debian-tdengine-driver:latest \
     aenjoy/debian-tdengine-driver-gobuilder:latest \
